fa90a60048997d1bc9f99e358979f11b02fa8f75
Pam Wang <pam_wang@Pams-Laptop.local>
2024-07-31 15:20:46 -0700

    start using test file

--- tests/test_operon_graph.py
+++ tests/test_operon_graph.py
@@ -15,7 +15,7 @@ def test_operon_graph():
         graph_output_path='data/test_data/test_GRAPH_Guaymas2020_hottest_clu30.gml',
     )
 
-    input_fasta = '/stor/work/Marcotte/project/drbarth/plastics/reference/Guaymas_mining/Guaymas2020/05.reference/Guaymas2020_Scaffolds_Bins_deduplicated_hottest_greaterthan80C.faa' # TODO: Choose smaller test file, possibly E. coli
+    input_fasta = '/Users/pam_wang/Documents/Projects/operon-graph/data/test_data/test_Prodigalheaders.fasta'#'/stor/work/Marcotte/project/drbarth/plastics/reference/Guaymas_mining/Guaymas2020/05.reference/Guaymas2020_Scaffolds_Bins_deduplicated_hottest_greaterthan80C.faa' # TODO: Choose smaller test file, possibly E. coli
     operon_graph = OperonGraph(config)
     G = operon_graph.run(input_fasta)
 
